412910ff2c35d097e76e3435e5152e8700282c6f
ingframin <framin2@gmail.com>
2020-04-04 19:29:19 +0200

    vsize not initialized fixed

--- Model.cpp
+++ Model.cpp
@@ -1,17 +1,16 @@
 #include "Model.h"
-
+#include<iostream>
 
 Model::Model(std::vector<GLfloat> vertices, std::vector<GLuint> elements)
 {
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-
+	
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vsize, &vertices[0], GL_STATIC_DRAW);
 	this->vertices = vertices;
 	this->vsize = vertices.size();
-
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vsize, &vertices[0], GL_STATIC_DRAW);
 	glGenBuffers(1, &ebo);
 	this->esize = elements.size();
 	this->elements = elements;
